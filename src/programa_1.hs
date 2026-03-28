c7bf60d4a685c27bc22b797f140ee4c58526e73a
CalebeAF02 <calebe.2324@gmail.com>
2026-03-28 12:19:47 -0300

    init(projeto): Iniciando a disciplina LP -  8 semestre - UnB

---
+++ src/programa_1.hs
@@ -0,0 +1,30 @@
+module Programa_1 where
+
+-- Autor: Calebe Alves Freitas 
+-- Data:  28/03/2026
+
+-- Uma função simples de soma
+soma :: Int -> Int -> Int
+soma x y = x + y
+
+-- Função que verifica se um número é par
+ehPar :: Int -> Bool
+ehPar n = mod n 2 == 0
+
+-- Exemplo usando a sintaxe de crase (infix) que você usou no prompt
+eDivisivel :: Int -> Int -> Bool
+eDivisivel x y = x `mod` y == 0
+
+-- Uma função que usa condicional (if-then-else)
+analisarNumero :: Int -> String
+analisarNumero n = if n > 0 
+                    then "Positivo" 
+                    else if n < 0 
+                        then "Negativo" 
+                        else "Zero"
+
+classificarNota :: Int -> String
+classificarNota nota
+    | nota >= 7  = "Aprovado"
+    | nota >= 5  = "Recuperacao"
+    | otherwise  = "Reprovado"
